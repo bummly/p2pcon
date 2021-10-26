2138131810882157687dd9b8d3e619d71c635aa0
Smitesh Das <57407736+smiteshdas@users.noreply.github.com>
2021-10-26 16:29:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
- peer = new Peer(null, {
+ const peer = new Peer(null, {
                         debug: 2
                     });
- conn = peer.connect(3245, {
+ letconn = peer.connect(3245, {
                         reliable: true
                     });
 
                     conn.on('open', function () {
-                        status.innerHTML = "Connected to: " + conn.peer;
-                        console.log("Connected to: " + conn.peer);
+                        
+                       document.write("Connected to: " + conn.peer);
 
                         // Check URL params for comamnds that should be sent immediately
-                        var command = window.location
                        
-                            conn.send(command);
+                       
+                            conn.send("Hey");
                     );
